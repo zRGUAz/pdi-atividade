7b4fe5e14eb0d69fb02161f3508fbfc427ba41ce
zRGUAz <rafael.gualdieri.pinheiro@gmail.com>
2025-03-19 15:47:47 -0300

    Atividade

--- q1/q1.py
+++ q1/q1.py
@@ -6,7 +6,6 @@ cap = cv2.VideoCapture("q1/q1A.mp4")
 def detect_shapes(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    # Intervalos de cores ajustados para melhor detecção
     color_ranges = {
         "red": [(np.array([0, 120, 70]), np.array([10, 255, 255])), (np.array([170, 120, 70]), np.array([180, 255, 255]))],
         "blue": [(np.array([90, 80, 50]), np.array([130, 255, 255]))],
@@ -20,11 +19,11 @@ def detect_shapes(frame):
         for lower, upper in ranges:
             mask |= cv2.inRange(hsv, lower, upper)
         
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))  # Remove ruídos
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8)) 
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
         for cnt in contours:
-            if cv2.contourArea(cnt) > 500:  # Evita ruídos
+            if cv2.contourArea(cnt) > 500:
                 x, y, w, h = cv2.boundingRect(cnt)
                 detected_shapes.append((color, (x, y, w, h)))
     
@@ -73,30 +72,24 @@ while True:
     collision_detected = check_collision(shapes)
     overlap_detected = check_overlap(largest_shape, shapes)
     
-    # Desenhar bounding boxes sem o texto
     for color, (x, y, w, h) in shapes:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
     
-    # Destacar maior forma
     if largest_shape:
         _, (x, y, w, h) = largest_shape
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-    # Mostrar mensagens
     if collision_detected:
         cv2.putText(frame, "COLISAO DETECTADA", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
     
     if overlap_detected:
         cv2.putText(frame, "ULTRAPASSAGEM DETECTADA", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
     
-    # Exibe resultado
     cv2.imshow("Feed", frame)
     
-    # Wait for key 'ESC' to quit
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
         break
 
-# That's how you exit
 cap.release()
 cv2.destroyAllWindows()
